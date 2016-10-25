8e4f874f0bd90425d4c2668f16823613965c53cf
Gabriel <gabriel.groener@gmail.com>
2016-10-25 17:58:36 +0200

    changed to work with SA
    made it worked with SA servers

--- user_part.sh
+++ user_part.sh
@@ -4,7 +4,7 @@
 
 mkdir -p ~/.ssh &&
 cat >> ~/.ssh/config << EOF
-Host vgs vgs.42.fr vgse vgse.42.fr vogsphere vogsphere.42.fr vogsphere-exam vogsphere-exam.42.fr
+Host vgs vgs.jnb.42.fr vgse vgse.jnb.42.fr vogsphere vogsphere.jnb.42.fr vogsphere-exam vogsphere-exam.jnb.42.fr
 	User vogsphere
 EOF
 
