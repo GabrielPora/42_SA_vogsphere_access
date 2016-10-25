cb4e0b214a471294febef512996d335dc5b6e9c3
Gabriel <gabriel.groener@gmail.com>
2016-10-25 17:55:54 +0200

    changed for SA
    changed it to work with SA servers

--- root_part.sh
+++ root_part.sh
@@ -4,7 +4,7 @@
 
 cat >> /etc/krb5.conf << EOF
 [libdefaults]
-	default_realm = 42.FR
+	default_realm = JNB.42.FR
 	forwardable = true
 	proxiable = true
       	dns_lookup_kdc = no
@@ -12,32 +12,24 @@ cat >> /etc/krb5.conf << EOF
 	allow_weak_crypto = true
 
 [realms]
-	42.FR = {
-                kdc = kdc1.42.fr
-                kdc = kdc2.42.fr
-                admin_server = kdc1.42.fr
-		default_domain = 42.fr
+	JNB.42.FR = {
+                kdc = kdc.jnb.42.fr
+                admin_server = kdc.jnb.42.fr
+		default_domain = jnb.42.fr
 		default_lifetime = 7d
                 ticket_lifetime = 7d
 	}
-        STAFF.42.FR = {
-                kdc = dc1.staff.42.fr
-                admin_server = dc1.staff.42.fr
-                default_domain = staff.42.fr
-        }
 
 [domain_realm]
-	.42.fr = 42.FR
-	42.fr = 42.FR
-	.staff.42.fr = STAFF.42.FR
-	staff.42.fr = STAFF.42.FR
+	.jnb.42.fr = JNB.42.FR
+	jnb.42.fr = JNB.42.FR
 EOF
 
 ### We set up the ssh config file to access to 42's git server (vogsphere)
 ### using your kerberos identification
 
 cat >> /etc/ssh/ssh_config << EOF
-Host *.42.fr
+Host *.jnb.42.fr
      SendEnv LANG LC_*
      StrictHostKeyChecking no
      GSSAPIAuthentication yes
